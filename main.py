2d677a0b3edf8b596ca0ba26479508217d8507f8
Fenomenon199 <afeldman2025@kehillahstudent.org>
2025-03-27 12:55:01 -0700

    5 questions

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-# Write your code here
+import random 
+flashcards= {
+   "How many people use an apple product in the world?" : " 2.2 billion " ,
+
+ " what are the lyrics to before he cheats by carrie underwood?" : "And i dug my key into the side of his pretty little suped up four wheel drive", 
+
+ " How many stars are there in the galaxy?" : " 100 to 400 billion stars ",
+
+ " How many animal rescues are there in the world? ": " 4,064 rescues " ,
+
+ "how many people are coffee drinkers in the world? " : " 1 billion people",
+}
+questions_to_reveiw= [] 
+questions= list (flashcards.keys()) 
+my_list= random.sample(questions, k=5 )
+for question in my_list: 
+    response = input (question)
+    if response == flashcards [question]: 
+        print ("✅") 
+    else: 
+        print (" ❌") 
+        questions_to_reveiw.append (f"{question}\n{flashcards [question]}") 
+print("reveiw these questions") 
+for QA in questions_to_reveiw: 
+    print (QA) 
